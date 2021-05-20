39f492246af2f5e25fa96a4afc07034984e48872
chayachhom <chayachhom@gmail.com>
2021-05-20 15:00:32 -0700

    Add files via upload

--- 11-dad-jokes/script.js
+++ 11-dad-jokes/script.js
@@ -1,15 +1,35 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
-jokeBtn.addEventListener('click', generateJoke)
+jokeBtn.addEventListener('click', newJoke)
 
+newJoke()
 
-generateJoke()
+async function newJoke() {
+    const config = {headers:{Accept:'application/json'}}
 
-function generateJoke() {
-    const config = {headers: {Accept: 'application/json'}}
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
 
-    fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => jokeEl.innerHTML = data.joke)    
+    jokeEl.innerHTML = data.joke
 }
+
+
+
+
+// const jokeEl = document.getElementById('joke')
+// const jokeBtn = document.getElementById('jokeBtn')
+
+// newJoke()
+
+// function newJoke() {
+// config = {headers:{Accept:'application/json'}}
+
+//     fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     })
+
+//     jokeBtn.addEventListener('click', newJoke)
+// }
